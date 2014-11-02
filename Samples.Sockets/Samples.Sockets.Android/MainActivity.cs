faaa98356a287e4cdea62c85d54327f3a318f542
Blacksun <angel.calvasp@outlook.com>
2014-11-02 18:09:39 -0500

    Added Soclet Proyect and Samples

---
+++ Samples.Sockets/Samples.Sockets.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+namespace Samples.Sockets.Droid
+{
+    [Activity(Label = "Samples.Sockets", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : AndroidActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+
+            SetPage(App.GetMainPage());
+        }
+    }
+}
+
---
+++ Samples.Sockets/Samples.Sockets.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Samples.Sockets.WinPhone.Resources;
+
+namespace Samples.Sockets.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
