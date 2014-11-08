0e813ad34276bb80898efb004b605047541b9b80
Blacksun <angel.calvasp@outlook.com>
2014-11-08 14:13:57 -0500

    Added Managers

---
+++ Blacksun.XamServices.Sockets.WinPhone/WindowsPhone8WebSocketManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using Blacksun.XamServices.Sockets.WinPhone;
+using WebSocket4Net;
+
+[assembly: Xamarin.Forms.Dependency(typeof(WindowsPhone8WebSocketManager))]
+namespace Blacksun.XamServices.Sockets.WinPhone
+{
+    public class WindowsPhone8WebSocketManager : IWebSocketManager
+    {
+
+        private WebSocket _webSocket = null;
+
+        public event EventHandler Closed;
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+        public event EventHandler Opened;
+
+        public Task Open(string host, int port)
+        {
+            var tcs = new TaskCompletionSource<string>();
+
+            _webSocket = new WebSocket("ws://" + host + ":" + port + "/");
+
+            _webSocket.Opened += (o, t) => OnOpened(t);
+            _webSocket.Closed += (o, t) => OnClosed(t);
+            _webSocket.Open();
+            _webSocket.DataReceived += (o, t) =>
+            {
+                var args = new DataReceivedEventArgs();
+                args.Data = t.Data;
+                OnDataReceived(args);
+            };
+            return tcs.Task;
+        }
+
+        protected virtual void OnOpened(EventArgs e)
+        {
+            if (Opened != null)
+            {
+                Opened(this, e);
+            }
+        }
+
+        public void Send(string message)
+        {
+            if (_webSocket != null)
+            {
+                _webSocket.Send(message);
+            }
+        }
+
+        protected virtual void OnDataReceived(DataReceivedEventArgs e)
+        {
+            if (DataReceived != null)
+            {
+                DataReceived(this, e);
+            }
+        }
+
+        protected virtual void OnClosed(EventArgs e)
+        {
+            if (Closed != null)
+            {
+                Closed(this,e);
+            }
+        }
+
+        public void Close()
+        {
+            if (_webSocket != null)
+            {
+                _webSocket.Close();
+            }
+        }
+    }
+}
---
+++ Blacksun.XamServices.Sockets/IWebSocketManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blacksun.XamServices.Sockets
+{
+    public interface IWebSocketManager
+    {
+
+        event EventHandler Closed;
+        event EventHandler<DataReceivedEventArgs> DataReceived;
+        event EventHandler Opened;
+
+        Task Open(string host, int port);
+        void Send(string message);
+        void Close();
+
+    }
+}
