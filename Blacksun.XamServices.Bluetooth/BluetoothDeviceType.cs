2dad9b8678e4744349c38b26d3376c04a58bd341
Blacksun <angel.calvasp@outlook.com>
2015-03-11 01:22:08 -0500

    Project fixed

---
+++ Blacksun.XamServices.Bluetooth/BluetoothDeviceType.cs
@@ -0,0 +1,23 @@
+﻿namespace Blacksun.Bluetooth
+{
+    public enum BluetoothDeviceType
+    {
+
+        // Summary:
+        //     Bluetooth device type, Unknown
+        Unknown = 0,
+        //
+        // Summary:
+        //     Bluetooth device type, Classic - BR/EDR devices
+        Classic = 1,
+        //
+        // Summary:
+        //     Bluetooth device type, Low Energy - LE-only
+        Le = 2,
+        //
+        // Summary:
+        //     Bluetooth device type, Dual Mode - BR/EDR/LE
+        Dual = 3,
+
+    }
+}
